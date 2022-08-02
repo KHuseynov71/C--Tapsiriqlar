c13842890a8d859bde3d55bb60afad657b62f40e
unknown <kamranmh@code.edu.az>
2022-08-02 16:51:40 +0400

    ilk c# tapsiriqi

---
+++ Tapsiriq/Tapsiriq/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Tapsiriq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                int min = 7;
+                int max = 77;
+                int i = 0;
+                while (min <= max)
+                {
+                    if (min % 7 == 0)
+                    {
+                        i++;
+                    }
+                    min++;
+                }
+                Console.WriteLine(i);
+                Console.ReadLine()
+            }
+        }
+    }
+}
---
+++ Tapsiriq2/Tapsiriq2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Tapsiriq2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string month = "February";
+            if (month == "January" || month == "March" || month == "May" || month == "July" || month == "August" || month == "October" || month == "December")
+            {
+                Console.WriteLine("31");
+            }
+            if (month == "April" || month == "June" || month == "September" || month == "November")
+            {
+                Console.WriteLine("30");
+            }
+            if (month == "February")
+            {
+                Console.WriteLine("28 or 29");
+            }
+
+        }
+    }
+}
